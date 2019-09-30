dde48eaf61ae4a2e48deea054e670b60779fcb4b
Karenina <karenizabel1999@gmail.com>
2019-09-29 20:41:21 -0600

    Creación del proyecto

---
+++ ED2-LAB2/Controllers/SerieIController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ED2_LAB2.Controllers
+{
+    public class SerieIController : Controller
+    {
+        // GET: SerieI
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CifradoZigZag()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CifradoZigZag(HttpPostedFileBase ArchivoImportado)
+        {
+            return View();
+        }
+        public ActionResult CifradoEspiaral()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CifradoEspiral(HttpPostedFileBase ArchivoImportado)
+        {
+            return View();
+        }
+        public ActionResult CifradoCesar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CifradoCesar(HttpPostedFileBase ArchivoImportado)
+        {
+            return View();
+        }
+
+    }
+}
