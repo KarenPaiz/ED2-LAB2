4a50c0c8e8d88a63625d847cdde1e6f35a0dae3a
Karenina <karenizabel1999@gmail.com>
2019-09-29 23:51:31 -0600

    Escritura de diccionario cifrado

--- ED2-LAB2/Controllers/SerieIController.cs
+++ ED2-LAB2/Controllers/SerieIController.cs
@@ -33,7 +33,32 @@ namespace ED2_LAB2.Controllers
         public ActionResult CifradoCesar(HttpPostedFileBase ArchivoImportado, string clave)
         {
             var DiccionarioCifrado = new Dictionary<char, char>();
-            
+            var Clave = clave.ToCharArray();
+            var ContadorAbecedario = 65; //Empieza en 'A' (65) y termina en 'z' (122)
+            foreach (var item in Clave)
+            {
+                if ( !(ContadorAbecedario >= 91 && ContadorAbecedario <= 96)) {
+                    if (!DiccionarioCifrado.ContainsValue(item)){
+                        DiccionarioCifrado.Add(Convert.ToChar(ContadorAbecedario), item);
+                        ContadorAbecedario++;
+                    }
+                }
+            }
+            for (int i = 65; i < 123; i++)
+            {
+                if (!(ContadorAbecedario >= 91 && ContadorAbecedario <= 96))
+                {
+                    if (!DiccionarioCifrado.ContainsValue(Convert.ToChar(i)) && !(i >= 91 && i <= 96))
+                    {
+                        DiccionarioCifrado.Add(Convert.ToChar(ContadorAbecedario), Convert.ToChar(i));
+                        ContadorAbecedario++;
+                    }
+                }
+                else {
+                    i--;
+                    ContadorAbecedario++;
+                }
+            }
             return View();
         }
 
